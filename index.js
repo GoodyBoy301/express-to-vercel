36b6eaccf0c5022018e4e11a800896850c32043d
Goodness Urama <87700025+GoodyBoy301@users.noreply.github.com>
2022-09-20 23:58:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
+app.get('/goody', (req, res) => {
+    res.send('working perfectly, Goody');
+})
 
 app.listen(process.env.PORT || 3000);
 
